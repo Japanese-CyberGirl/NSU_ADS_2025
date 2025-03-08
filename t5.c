de19e8e0d4b4bfdb169e21c37b1453175cdb5ac0
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-03-08 14:39:37 +0700

    task 5 accepted

--- t5.c
+++ t5.c
@@ -2,66 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 
+int compare(char *string, char *token) {
+    int len1 = strlen(string);
+    int len2 = strlen(token);
+
+    int len = (len1 < len2) ? len1 : len2;
+
+    for (int i = 0; i < len; i++) {
+        if (string[i] > token[i]) {
+            return 1;
+        }
+        if (string[i] < token[i]) {
+            return -1;
+        }
+    }
+
+    if (len1 < len2) {
+        return -1;
+    }
+    if (len1 > len2) {
+        return 1;
+    }
+
+    return 0;
+}
+
+
 typedef struct node {
-    int value;
+    char *value;
     struct node *left;
     struct node *right;
 } tree;
 
-tree *add(tree *t , int x) {
+tree *add(tree *t, char *s) {
     if (t == NULL) {
         t = (tree*)calloc(1, sizeof(tree));
-        t->value = x;
+        t->value = (char*)calloc(101, sizeof(char));
+        strcpy(t->value, s); 
         t->left = NULL;
         t->right = NULL;
         return t;
     }
-    if (t->value > x) {
-        t->left = add(t->left, x);
+    if (compare(t->value, s) == 1) {
+        t->left = add(t->left, s);
     }
-    else {
-        if (t->value < x) {
-            t->right = add(t->right, x);
-        }
+    else if (compare(t->value, s) == -1) {
+        t->right = add(t->right, s);
     }
     return t;
 }
 
-int compare(const char *string, const char *token) {
-    int len1 = strlen(string);
-    int len2 = strlen(token);
-    int len = (len1 < len2) ? len1 : len2; 
-
-    for (int i = 0; i < len; i++) {
-        if (string[i] > token[i]) {
-            return 1;  // string больше
-        }
-        if (string[i] < token[i]) {
-            return -1; // token больше
-        }
+void infix(tree *t) {
+    if (t == NULL) {
+        return;
     }
-
-  
-    if (len1 < len2) {
-        return -1; 
-    }
-    if (len1 > len2) {
-        return 1; 
-    }
-
-    return 0; 
+    infix(t->left);
+    printf("%s\n", t->value);
+    infix(t->right);
 }
 
-
-int main()
-{
-
+int main() {
     FILE *input = fopen("input.txt", "r");
 
-    printf("%d\n", compare("abd", "abc"));
+    tree *root = NULL;
+    char token[101]; 
+    while (fscanf(input, "%s", token) == 1) {
+        root = add(root, token);
+    }
 
-
-
+    infix(root); 
     fclose(input);
     return 0;
 }
