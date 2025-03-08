96688b00e99a97b0ece76df0cc69863d04a5d036
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-03-08 19:20:54 +0700

    i'm tired to come over with this fucking idiot shit i hate it blyat

--- t1.c
+++ t1.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define table_size 1000003 //размеры нашей хеш-таблицы
+
+//структура для узла в цепочке для метода цепочек
+struct Node {
+    int key;
+    struct Node *next;
+};
+
+struct Hashtable {
+    struct Node *table[table_size];
+};
+
+unsigned int hash (int key) {
+    return (unsigned int)key % table_size;
+}
+
+void create_hash_table(struct Hashtable *ht) {
+    for (int i = 0 ; i < table_size ; i ++ ) {
+        ht->table[i] = NULL;
+    }
+}
+
+
+
+
 int main()
 {
     return 0;
