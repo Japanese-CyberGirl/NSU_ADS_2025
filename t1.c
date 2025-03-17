4a4c4c8163b93dce021c453eb91174358cc90df7
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-03-17 19:24:44 +0700

    it's still a lot i have to do

--- t1.c
+++ t1.c
@@ -5,66 +5,8 @@ typedef struct Tree {
     int value;
     struct Tree *left;
     struct Tree *right;
+    struct Tree *parent;
 } Tree;
-
-Tree *create(int value) {
-    Tree *new = (Tree*)calloc(1, sizeof(Tree));
-    new->value = value;
-    new->left = NULL;
-    new->right = NULL;
-    return new;
-}
-
-Tree *insert(Tree *tree, int value) {
-    if (tree = NULL) {
-        return create(value);
-    }
-    
-    if (value <= tree->value) { //нестрогое неравенство для сохранения дубликатов
-        tree->left = insert(tree->left, value);
-    }
-    if (value > tree->value) {
-        tree->right = insert(tree->right, value);
-    }
-
-    return tree;
-}
-
-Tree *search(Tree *tree, int value) {
-    if (tree == NULL || tree->value == value) {
-        return tree;
-    }
-
-    if (value < tree->value) {
-        return search(tree->left, value);
-    }   else {
-        return search(tree->left, value);
-    }
-
-}
-
-Tree *delete(Tree *tree, int value) {
-    if (tree == NULL) {
-        return tree;
-    }
-
-    if (value < tree->value) {
-        tree->left = delete(tree->left, value);
-    } else if (value > tree->value) {
-        tree->right = delete(tree->right, value);
-    } else {
-        //если дальше нет узлов
-        if (tree->left == NULL) {
-            Tree *temp = tree->right;
-            return temp;
-        } else if (tree->right == NULL) {
-            Tree *temp = tree->left;
-            return temp;
-        }
-    }
-}
-
-
 
 
 int main()
