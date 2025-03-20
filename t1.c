64c325b610f6cff44c39e82e9f0de26e027576f8
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-03-20 11:50:29 +0700

    start working on the red-black tree

--- t1.c
+++ t1.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Tree {
-    int value;
-    struct Tree *left;
-    struct Tree *right;
-    struct Tree *parent;
-} Tree;
+typedef struct Node {
+    int key;
+    char color;
+    struct Node *left;
+    struct Node *right;
+    struct Node *parent;
+    int counter;
+} Node;
 
+Node *creation(int key) {
+    Node *new = (Node*)calloc(1, sizeof(Node));
+    new->key = key;
+    new->color = 'R';
+    new->left = NULL;
+    new->right = NULL;
+    new->parent = NULL;
+    return new;
+}
+
+void left_rotation(Node *root, Node *x) {
+    Node *y = x->right; // y - правый потомок х
+    x->right = y->left; // левый потомок превращается в правого потомка x
+    if (y->left != NULL) {
+        y->left->parent = x;
+    }
+
+    y->parent = x->parent;
+    if (x->parent == NULL) {
+        *root = y;
+    }
+    else if(x == x->parent->left) x->parent->left = y;
+    else x->parent->right = y;
+
+    y->left = x;
+    x->parent = y;
+}
 
 int main()
 {
