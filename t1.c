50a3f9c5392622be4483a0ca479ddd07783ab857
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-03-22 03:03:48 +0700

    working on task 1

--- t1.c
+++ t1.c
@@ -29,24 +29,62 @@ int Right(int i) {
     return (2 * i + 2);
 }
 
-void heapiDown(Heap *h, int i) {
-    int largest = i;
-    int l = left(i);
-    int r = right(i);
+void heapifyDown(Heap *h, int i) {
+    int smallest = i;
+    int l = Left(i);
+    int r = Right(i);
 
-    if (l < h->size && h->heap[l] > h->heap[largest]) {
-        largest = l;
-    }
-    if (r < h->size && h->heap[r] > h->heap[largest]) {
-        largest = r;
+    if (l < h->size && h->heap[l] < h->heap[smallest]) {
+        smallest = l;
     }
 
-    if (largest != i) {
+    if (r < h->size && h->heap[r] < h->heap[smallest]) {
+        smallest = r;
+    }
+
+    if (smallest != i) {
         int temp = h->heap[i];
-        h->heap[i] = h->heap[largest];
-        h->heap[largest] = temp;
-        heapiDown(h, largest);
+        h->heap[i] = h->heap[smallest];
+        h->heap[smallest] = temp;
 
+        heapifyDown(h, smallest);
     }
 }
 
+void heapifyUp(Heap *h, int i) {
+    while (i != 0 && h->heap[Parent(i)] > h->heap[i]) {
+        int temp = h->heap[i];
+        h->heap[i] = h->heap[Parent(i)];
+        h->heap[Parent(i)] = temp;
+        i = Parent(i);
+    }
+}
+
+void push(Heap *h, int key) {
+    h->heap[h->size] = key;
+    h->size ++;
+    heapifyUp(h, h->size - 1);
+}
+
+int pop(Heap *h) {
+    int root = h->heap[0];
+    h->heap[0] = h->heap[h->size - 1];
+    h->size --;
+    heapifyDown(h, 0);
+    return root;
+}
+
+int main()
+{
+    FILE *input = fopen("input.txt", "r");
+    int Q = 0;
+    fscnaf(input, "%d", &Q);
+    Heap *heap = create(Q);
+
+    for (int i = 0 ; i < Q ; i ++ ) {
+        char operation[10];
+        fscanf(input, "%s",&operation);
+    }
+
+    return 0;
+}
