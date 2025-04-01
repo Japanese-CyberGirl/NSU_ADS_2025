f0a4bed6ca54268b98bdc18cb0159f506b5c6faa
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-04-01 22:56:08 +0700

    svyat task accepted

--- svyat.c
+++ svyat.c
@@ -3,19 +3,32 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+bool containsAnyChar(const char* str, const char* substr) {
+    if (!str || !substr) return false;
+    for (int i = 0; substr[i]; i++) {
+        if (strchr(str, substr[i]))  
+            return true;
+    }
+    return false;
+}
 
-//есть строка, слова разделены пробелами, вывести на экран все слова, которые не содержат тех же символов, что есть в первом слове.
-
-int main()
-{
+int main() {
     FILE *input = fopen("input.txt", "r");
 
     char *string = (char*)calloc(1000000, sizeof(char));
-
-    while(fscanf(input, "%s", string) == 1) {
-        printf("%s\n", string);
-    }
+    fgets(string, 1000000, input);
 
     fclose(input);
+
+    char *firstWord = strtok(string, " \n");
+
+    char *word = strtok(NULL, " \n");
+    while (word) {
+        //printf("%s\n", word);
+        if (!containsAnyChar(firstWord, word)) printf("%s\n", word);
+        word = strtok(NULL, " \n");
+    }
+
+    free(string);
     return 0;
 }
