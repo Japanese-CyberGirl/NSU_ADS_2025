f3c0cbfe8fef01253a1656895abe93c2c7d3f6a5
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-04-11 20:56:10 +0700

    first task accepted

--- t1.c
+++ t1.c
@@ -2,60 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Queue_item {
-    int where;
-    int from_where;
-} Queue_item;
-
-typedef struct Queue {
-    int size;
-    int first;
-    int leng;
-    Queue_item *arr;
-} Queue;
-
-Queue *create() {
-    Queue *q = (Queue*)calloc(1, sizeof(Queue));
-    q->first = 0;
-    q->leng = 0;
-    q->size = 1;
-    q->arr = (Queue_item*)calloc(q->size, sizeof(Queue_item));
-    return q;
-}
-
-int empty(Queue *q) {
-    return (q->leng == 0);
-}
-
-void enqueue(Queue *q, int where, int from_where) {
-    if (q->leng == q->size) {
-        q->arr = (Queue_item*)realloc(q->arr, sizeof(Queue_item) * q->size * 2);
-        if (q->first > 0) {
-            memcpy(q->arr + q->size, q->arr, (q->first) * sizeof(Queue_item));
+void print_matrix(int **matrix, int N) {
+    for (int i = 0 ; i < N ; i ++ ) {
+        for (int j = 0 ; j < N ; j ++ ) {
+            printf("%d\t", matrix[i][j]);
         }
-        q->size *= 2;
+        printf("\n");
     }
-    int pos = (q->first + q->leng) % q->size;
-    q->arr[pos].where = where;
-    q->arr[pos].from_where = from_where;
-    q->leng ++ ;
 }
 
-Queue_item dequeue(Queue *q) {
-    Queue_item a = q->arr[q->first];
-    q->first = (q->first + 1) % q->size;
-    q->leng--;
-    return a;
+int zero_column(int **matrix, int N) {
+    int temp = 1;
+    for (int j = 0 ; j < N ; j ++ ) {
+        for (int i = 0 ; i < N ; i ++ ) {
+            if (matrix[i][j] != 0) temp = 0;
+        }
+        if (temp) {
+            for (int a = 0 ; a < N ; a ++ ) {
+                matrix[a][j] = 52;
+            }
+            return j;
+        }
+        temp = 1;
+    }
+    return -1;
 }
 
-
+void row_to_zero(int **matrix, int N, int x) {
+    for (int i = 0 ; i < N ; i ++ ) {
+        for (int j = 0 ; j < N ; j ++ ) {
+            if (i == x) matrix[i][j] = 0;
+        }
+    }
+}
 
 int main()
 {
     FILE *input = fopen("input.txt", "r");
-
-    Queue *queue = create();
-
 
     int N = 0;
     int M = 0;
@@ -73,21 +56,26 @@ int main()
         fscanf(input, "%d %d", &inp, &out);
         matrix[inp-1][out-1] = 1;
     }
+    //matrix[i][j] - i строка j столбец
+    int *topological_array = (int*)calloc(N, sizeof(int));
 
     for (int i = 0 ; i < N ; i ++ ) {
-        for (int j = 0 ; j < N ; j ++ ) {
-            printf("%d\t", matrix[i][j]);
+        //print_matrix(matrix, N);
+        //printf("\n");
+        int x = zero_column(matrix, N);
+        if (x == -1) {
+            printf("bad course\n");
+            return 0;
         }
-        printf("\n");
+        //printf("%d\n\n", x);
+        topological_array[i] = x+1;
+        row_to_zero(matrix, N, x);
     }
 
+    ////print_matrix(matrix, N);
+    //printf("\n");
     for (int i = 0 ; i < N ; i ++ ) {
-        for (int j = 0 ; j < N ; j ++) {
-            if (matrix[i][j] == 1) {
-                break;
-            }
-        }
-        enqueue(queue, )
+        printf("%d\t", topological_array[i]);
     }
 
     fclose(input);
