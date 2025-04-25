2ea1863dd7f52e340dbd1855016fa54371a392b4
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-04-25 23:04:46 +0700

    working

--- t1.c
+++ t1.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+void search(int v , bool *visited , int **matrix, int vertex_counter) {
+    visited[v] = true;
+    printf("%d \n", v);
+
+    for (int i = 0 ; i < vertex_counter ; i ++ ) {
+        if (matrix[v][i] && !visited[i]) {
+            search(i , visited , matrix, vertex_counter);
+        }
+    }
+}
+
+void DFS(int **matrix , int vertex_counter , int start_vertex) {
+    bool *visited = (bool*)calloc(vertex_counter, sizeof(bool));
+
+    search(start_vertex, visited, matrix, vertex_counter);
+}
 
 int main()
 {
 
     FILE *input = fopen("input.txt", "r");
 
-    int N = 0;
+    int N = 0; //vertex_counter;
     int M = 0;
     fscanf(input, "%d %d", &N, &M);
+    printf("%d %d\n", N, M);
 
-    int **matrix = (int**)calloc(N, sizeof(int*));
-    for (int i = 0 ; i < M ; i ++ ) {
-        matrix[i] = (int*)calloc(N, sizeof(int));
-    }
-
-    for (int i = 0 ; i < M ; i ++ ) {
-        int i_cor = 0;
-        int j_cor = 0;
-        fscanf(input, "%d %d", &i_cor, &j_cor);
-        matrix[i_cor][j_cor] = 1;
-        matrix[j_cor][i_cor] = 1;
-    }
+    
 
     fclose(input);
     return 0;
