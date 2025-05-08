89deee83ce87f5c3ec48833917e7f28c70942fff
Japanese-CyberGirl <chinese_anime_girl@mail.ru>
2025-05-08 13:26:10 +0700

    task 1 accepted

--- t1.c
+++ t1.c
@@ -1,56 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 typedef struct Node {
     int vertex;
-    struct Node *next;
+    struct Node* next;
 } Node;
 
-typedef struct Graph {
-    int vertex_count;
-    struct Node **adjacent;
-} Graph;
+Node** adj;
+int* visited;
+int* onStack;
+int* path;
+int pathTop = -1;
+int cycleFound = 0;
+int* cycle;
+int cycleSize = 0;
 
-struct Node *create_Node(int vertex) {
-    struct Node *new_Node = (struct Node*)calloc(1, sizeof(struct Node));
-    new_Node->vertex = vertex;
-    new_Node->next = NULL;
-    return new_Node;
-} 
-
-struct Graph *create_Graph(int num_of_vertices) {
-    struct Graph *graph = (struct Graph*)calloc(num_of_vertices, sizeof(Graph));
-    graph->vertex_count = num_of_vertices;
-    graph->adjacent = (struct Node**)calloc(num_of_vertices + 1, sizeof(Node*));
-
-    for (int i = 0 ; i < num_of_vertices ; i ++ ) {
-        graph->adjacent[i] = NULL;
-    }
-    return graph;
+void push(int vertex) {
+    path[++pathTop] = vertex;
 }
 
-void add_Edge(struct Graph *graph, int source, int destination) {
-    struct Node *new_Node = create_Node(destination);
-    new_Node->next = graph->adjacent[source];
-    graph->adjacent[source] = new_Node;
+void pop() {
+    pathTop--;
 }
 
-void print_graph(struct Graph *graph) {
-    for (int v = 0; v < graph->vertex_count ; v ++ ) {
-        struct Node *temp = graph->adjacent[v];
-        printf("Vertex %d: ", v);
-        while (temp) {
-            printf("%d -> ", temp->vertex);
-            temp = temp->next;
+void addEdge(int u, int v) {
+    Node* newNode = (Node*)calloc(1, sizeof(Node));
+    newNode->vertex = v;
+    newNode->next = adj[u];
+    adj[u] = newNode;
+}
+
+void DFS(int start, int n) {
+    int* stack = (int*)calloc(n, sizeof(int));
+    int stackSize = 0;
+    int* indexStack = (int*)calloc(n, sizeof(int));
+    int indexStackSize = 0;
+
+    stack[stackSize++] = start;
+    visited[start] = 1;
+    push(start);
+    onStack[start] = 1;
+
+    while (stackSize > 0 && !cycleFound) {
+        int u = stack[stackSize - 1];
+        Node* node = adj[u];
+        int found = 0;
+
+        while (node != NULL) {
+            int v = node->vertex;
+            if (!visited[v]) {
+                visited[v] = 1;
+                stack[stackSize++] = v;
+                push(v);
+                onStack[v] = 1;
+                found = 1;
+                break;
+            } else if (onStack[v]) {
+                cycleSize = 0;
+                int idx = pathTop;
+                while (path[idx] != v) idx--;
+                cycleSize = pathTop - idx + 1;
+                cycle = (int*)calloc(cycleSize, sizeof(int));
+                for (int i = idx; i <= pathTop; i++) {
+                    cycle[i - idx] = path[i];
+                }
+                cycleFound = 1;
+                return;
+            }
+            node = node->next;
         }
-        printf("NULL\n");
+
+        if (!found) {
+            stackSize--;
+            pop();
+            onStack[u] = 0;
+        }
     }
+
+    free(stack);
+    free(indexStack);
 }
 
+int main() {
+    FILE* input = fopen("input.txt", "r");
+    int n, m;
+    fscanf(input, "%d %d", &n, &m);
 
-int main()
-{
+    adj = (Node**)calloc(n + 1, sizeof(Node*));
+    visited = (int*)calloc(n + 1, sizeof(int));
+    onStack = (int*)calloc(n + 1, sizeof(int));
+    path = (int*)calloc((n + 1), sizeof(int));
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        fscanf(input, "%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    for (int u = 1; u <= n && !cycleFound; u++) {
+        if (!visited[u]) {
+            DFS(u, n);
+        }
+    }
+
+    if (cycleFound) {
+        printf("%d\n", cycleSize);
+        for (int i = 0; i < cycleSize; i++) {
+            printf("%d ", cycle[i]);
+        }
+        printf("\n");
+    } else {
+        printf("-1\n");
+    }
+
+    fclose(input);
+    for (int i = 1; i <= n; i++) {
+        Node* node = adj[i];
+        while (node != NULL) {
+            Node* temp = node;
+            node = node->next;
+            free(temp);
+        }
+    }
+    free(adj);
+    free(visited);
+    free(onStack);
+    free(path);
+    if (cycleFound) free(cycle);
 
     return 0;
 }
